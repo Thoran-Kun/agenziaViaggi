805ddcc2f24cf91a0dd74d059146b00ee5cc27d3
Salvatore <pepesalvatore12@gmail.com>
2026-02-13 09:58:32 +0100

    Initial commit

---
+++ src/main/java/com/salvatore/agenziaViaggi/AgenziaViaggiApplication.java
@@ -0,0 +1,13 @@
+package com.salvatore.agenziaViaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenziaViaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenziaViaggiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salvatore/agenziaViaggi/AgenziaViaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvatore.agenziaViaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenziaViaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
